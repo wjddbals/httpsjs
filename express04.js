c3cec071e754ed404a68be1c694c6ccbf223bede
FULLSTACK <jumin0220@naver.com>
2023-02-16 17:48:38 +0900

    성적처리-1-body-parser

--- express04.js
+++ express04.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const logger=require('morgan');//로그출력기
 const {engine} =require('express-handlebars');
+const bodyParser=require('body-parser');//폼처리기
 
 //라우팅 외부 작성
 const indexRouter = require('./routes/index');
@@ -12,10 +13,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
+
 //view템프릿 엔진 설정
 app.engine('hbs',engine({ //hbs엔진으로 이름
     extname:'.hbs',
-    defaultLayout:'layout', //디폴트 서정안하면 메인으로 뜬다
+    defaultLayout:'layout', //디폴트 설정안하면 메인으로 뜬다
     helpers: {
         section: function(name, options) {
             if(!this._sections) this._sections = {}
@@ -34,6 +36,13 @@ app.use(express.static(path.join(__dirname,'static')));
 
 //로그설정
 app.use(logger('dev'));
+
+//미들웨어 등록
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));//전송데이터에 대한 urlencoding위한 설정
+app.use(bodyParser.json()); //전송된 폼데이터는json형식
+//app.use(bodyParser.raw()); //
+//app.use(bodyParser.text()); //enctype이 text/plain일때 필요 비추
 
 // index에 대한 route handler 지정
 app.use('/', indexRouter);
@@ -62,3 +71,5 @@ app.listen(port,() =>{
     console.log('express 서버가 실행중... 중지하혀면 ctrl+c를 눌러주세요!');
 });
 
+//라우트 메소드는 HTTP 메소드 중 하나로부터 파생되며, express 클래스의 인스턴스에 연결됩니다.
+// 다음 코드는 앱의 루트에 대한 GET 및 POST 메소드에 대해 정의된 라우트의 예입니다.
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,35 @@ const path = require("path");
 //const html='text/html;charset=utf-8';
 //show index page
 router.get('/',(req, res)=>{
-   //
-    // res.sendFile(path.join(__dirname,'../public','index.html'));
-    //handlebars 뷰 엔진으로 응답처리
     res.render('index',{title:'index'});
 
 });
+
+router.get('/sungjuk',(req, res)=>{
+    res.render('sungjuk',{title:'성적처리'});
+
+});
+
+router.post('/sungjuk',(req, res,next)=>{
+    //폼전송된 데이터들은req.body, req.body.폼이름 등으로로 확인가는
+    //console.log(req.body);
+   // console.log(req.body.name,req.body.kor,req.body.mat,req.body.eng);
+    let {name,kor,eng,mat} =req.body;
+    console.log(name,kor,eng,mat);
+
+    //성적처리
+    let [tot,avg,grd] =[kor+eng+mat, (kor+eng+mat)/3,'가'];
+    console.log(tot,avg,grd);
+
+
+    //데이터베이스 처리-sungjuk테이블에 insert
+
+
+
+    res.redirect(304,'/');
+
+});
+
 
 
 //던슌헌 그림파잉을 화면에 표시하기 위해
